b0b44f2ea3778f0ff356c7ed7fb853e7c83bd05a
Fedulov Danil <danya7777kg@gmail.com>
2026-03-14 21:01:40 +0600

    Установил multer и настроил его

--- api/index.ts
+++ api/index.ts
@@ -7,6 +7,7 @@ const port = 8000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 const run = async () => {
     await mongoose.connect('mongodb://localhost/test');
