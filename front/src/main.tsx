964355b5de6934ef4dee54ad142568951bc3206d
Fedulov Danil <danya7777kg@gmail.com>
2026-03-24 20:51:42 +0600

    [hw-85] init App

---
+++ front/src/main.tsx
@@ -0,0 +1,20 @@
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App.tsx';
+import {BrowserRouter} from 'react-router-dom';
+import {CssBaseline} from '@mui/material';
+import {ToastContainer} from 'react-toastify';
+import {Provider} from 'react-redux';
+import {store} from './app/store.ts';
+
+createRoot(document.getElementById('root')!).render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <StrictMode>
+                <CssBaseline />
+                <App />
+                <ToastContainer />
+            </StrictMode>
+        </BrowserRouter>
+    </Provider>,
+)
